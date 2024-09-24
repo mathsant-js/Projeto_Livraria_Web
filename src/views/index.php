7ed2c67898230a7719bf4fe06597a77dabe2fd2d
mathsant-js <matheus.santana114@etec.sp.gov.br>
2024-09-24 11:53:49 -0300

    fix: links to bootstrap style and functions and node settings have been fixed

--- src/views/index.php
+++ src/views/index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="assets/scss/main.css">
-    <script src="/src/js/bootstrap/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="../assets/scss/main.css">
+    <script src="../js/bootstrap/bootstrap.bundle.min.js"></script>
 </head>
 <body>
 </body>
