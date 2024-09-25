1bf4e49de02ea6c9ccfc94458c6c0c8e5e0b41a1
mathsant-js <matheus.santana114@etec.sp.gov.br>
2024-09-25 08:59:25 -0300

    feat: component header was added in index.php

--- src/components/header.php
+++ src/components/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Open Book</title>
     <link rel="stylesheet" href="../assets/scss/main.css">
     <script src="../js/bootstrap/bootstrap.bundle.min.js"></script>
 </head>
--- src/views/index.php
+++ src/views/index.php
@@ -1,3 +1,4 @@
+<?php require_once '../components/header.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
