5ee86b76235352124560ef0d7390f41dd1807380
matheussantana2007 <matheus.santana114@etec.sp.gov.br>
2024-09-28 12:56:56 -0300

    feat: carousel on homepage has been added

--- src/views/index.php
+++ src/views/index.php
@@ -1,6 +1,7 @@
 <?php require_once '../components/header.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +9,46 @@
     <link rel="stylesheet" href="../assets/scss/main.css">
     <script src="../js/bootstrap/bootstrap.bundle.min.js"></script>
 </head>
-<body>
+
+<body class="background-dark-light">
+    <div id="carouselExampleCaptions" class="carousel slide container mt-5">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="../assets/imgs/static/imgCarousel1.svg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-warning fs-4">Livros com ótimos preços, sem sair de casa!</h5>
+                    <p>Na livraria, você compra seus livros favoritos por preços baixos e justos, e pede para entrega no conforto de sua casa!</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/imgs/static/imgCarousel1.svg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-warning">Second slide label</h5>
+                    <p>Some representative placeholder content for the second slide.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/imgs/static/imgCarousel1.svg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-warning">Third slide label</h5>
+                    <p>Some representative placeholder content for the third slide.</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
 </body>
+
 </html>
