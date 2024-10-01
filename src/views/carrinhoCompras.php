c748a1e801fbedb0b2e0ed197621c2334fea8eba
mathsant-js <matheus.santana114@etec.sp.gov.br>
2024-10-01 12:34:42 -0300

    feat: carrinhoCompras page has been added

---
+++ src/views/carrinhoCompras.php
@@ -0,0 +1,39 @@
+<?php require_once '../components/header.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carrinho de Compras</title>
+    <link rel="stylesheet" href="../assets/scss/main.css">
+    <script src="../js/bootstrap/bootstrap.bundle.min.js"></script>
+</head>
+<body class="background-dark-light">
+<div class="container text-start mt-5">
+  <div class="row">
+    <div class="col">
+      <h2 class="lexend-title-semibold text-warning">Carrinho de Compras</h2>
+      <div class="shadow py-3 ps-3 mt-5 mb-5 text-warning bg-dark rounded">
+      <div class="row align-items-start">
+    <div class="col">
+      <img src="https://placehold.co/100" alt="">
+    </div>
+    <div class="col-9">
+      <p>Nome do Livro: livro</p>
+      <p class="text-light">Autor: autor</p>
+      <p class="text-light">Editora: editora</p>
+      <p class="lexend-text-medium fs-5">R$ XX,XX</p>
+    </div>
+    <div class="col">
+        <a href="" class="btn btn-warning btn-lg text-light">Ver Produto</a>
+        <a href="" class="btn btn-primary btn-lg text-light">sass</a>
+    </div>
+      </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+<?php require_once '../components/footer.php' ?>
+</html>
