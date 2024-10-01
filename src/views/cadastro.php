ed27237bfb8cab7309d0d95fe03be24d687fce86
matheussantana2007 <matheus.santana114@etec.sp.gov.br>
2024-09-30 21:41:20 -0300

    fix: floating labels has been fixed

--- src/views/cadastro.php
+++ src/views/cadastro.php
@@ -20,54 +20,48 @@
                 <h2 class="text-warning text-center">Seja bem-vindo à Open Book!</h2>
                 <p class="text-white text-center">Preencha os dados solicitados para criar sua conta.</p>
                 <p class="text-white text-center mt-2">Já possui uma conta? <a href="" class="link-warning">Entre agora.</a></p>
-                <div class="mb-3 form-floating">
+
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control text-warning background-dark-light border-bottom border-top-0 border-start-0 border-end-0 border-3 rounded-bottom-0 border-warning me-2" id="nome" name="nome" placeholder="Nome">
                     <label for="nome">Nome</label>
-                    <input type="text" class="form-control background-dark-light text-warning border-bottom border-top-0 border-start-0 border-end-0 border-3 rounded-bottom-0 border-warning me-2" id="nome" name="nome">
                 </div>
-
-                <div class="mb-3 form-floating">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control text-warning background-dark-light border-bottom border-top-0 border-start-0 border-end-0 border-3 rounded-bottom-0 border-warning me-2" id="cpf" name="cpf" placeholder="CPF">
                     <label for="cpf">CPF</label>
-                    <input type="text" class="form-control background-dark-light text-warning border-bottom border-top-0 border-start-0 border-end-0 border-3 rounded-bottom-0 border-warning me-2" id="cpf" name="cpf">
                 </div>
-
-                <div class="mb-3 form-floating">
-                    <label for="datanasc">Data de Nascimento</label>
-                    <input type="date" class="form-control background-dark-light text-warning border-bottom border-top-0 border-start-0 border-end-0 border-3 rounded-bottom-0 border-warning me-2" id="datanasc" name="datanasc">
+                <div class="form-floating mb-3">
+                    <input type="date" class="form-control text-warning background-dark-light border-bottom border-top-0 border-start-0 border-end-0 border-3 rounded-bottom-0 border-warning me-2" id="datanasc" name="datanasc" placeholder="Data de Nascimento">
+                    <label for="datenasc">Data de Nascimento</label>
                 </div>
-
-                <div class="mb-3 form-floating">
+                <div class="form-floating mb-3">
+                    <input type="email" class="form-control text-warning background-dark-light border-bottom border-top-0 border-start-0 border-end-0 border-3 rounded-bottom-0 border-warning me-2" id="email" name="email" placeholder="Email">
                     <label for="email">Email</label>
-                    <input type="email" class="form-control background-dark-light text-warning border-bottom border-top-0 border-start-0 border-end-0 border-3 rounded-bottom-0 border-warning me-2" id="email" name="email">
                 </div>
-
-                <div class="mb-3 form-floating">
-                    <label for="telefone">Telefone</label>
-                    <input type="text" class="form-control background-dark-light text-warning border-bottom border-top-0 border-start-0 border-end-0 border-3 rounded-bottom-0 border-warning me-2" id="telefone" name="telefone">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control text-warning background-dark-light border-bottom border-top-0 border-start-0 border-end-0 border-3 rounded-bottom-0 border-warning me-2" id="telefone" name="telefone" placeholder="Telefone">
+                    <label for="text">Telefone</label>
                 </div>
-
-                <div class="mb-3 form-floating">
-                    <label for="endereco">Endereço</label>
-                    <input type="text" class="form-control background-dark-light text-warning border-bottom border-top-0 border-start-0 border-end-0 border-3 rounded-bottom-0 border-warning me-2" id="endereco" name="endereco">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control text-warning background-dark-light border-bottom border-top-0 border-start-0 border-end-0 border-3 rounded-bottom-0 border-warning me-2" id="endereco" name="endereco" placeholder="Endereço">
+                    <label for="text">Endereço</label>
                 </div>
-
-                <div class="mb-3 form-floating">
+                <div class="form-floating mb-3">
+                    <input type="password" class="form-control text-warning background-dark-light border-bottom border-top-0 border-start-0 border-end-0 border-3 rounded-bottom-0 border-warning me-2" id="nome" name="nome" placeholder="Senha">
                     <label for="senha">Senha</label>
-                    <input type="password" class="form-control background-dark-light text-warning border-bottom border-top-0 border-start-0 border-end-0 border-3 rounded-bottom-0 border-warning me-2" id="senha" name="senha">
                 </div>
-
-                <div class="mb-3 form-floating">
-                    <label for="confsenha">Confirmar senha</label>
-                    <input type="password" class="form-control background-dark-light text-warning border-bottom border-top-0 border-start-0 border-end-0 border-3 rounded-bottom-0 border-warning me-2" id="confsenha" name="confsenha">
+                <div class="form-floating mb-3">
+                    <input type="password" class="form-control text-warning background-dark-light border-bottom border-top-0 border-start-0 border-end-0 border-3 rounded-bottom-0 border-warning me-2" id="confsenha" name="confsenha" placeholder="Confirmar Senha">
+                    <label for="confsenha">Confirmar Senha</label>
                 </div>
-
                 <div class="w-100 text-center">
-                <button type="submit" class="btn btn-warning btn-lg text-white text-center m-3">Cadastrar</button>
+                    <button type="submit" class="btn btn-warning btn-lg text-white text-center m-3">Cadastrar</button>
                 </div>
             </form>
         </div>
     </div>
 
-    
+
 </body>
 <?php require_once '../components/footer.php'; ?>
+
 </html>
