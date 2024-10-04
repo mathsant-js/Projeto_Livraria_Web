b91a0e7e85ab570f9f6d3c541df9179b67c646c8
matheussantana2007 <matheus.santana114@etec.sp.gov.br>
2024-10-04 15:23:31 -0300

    feat: carrinhoCompras.php page has been finished

--- src/views/carrinhoCompras.php
+++ src/views/carrinhoCompras.php
@@ -1,39 +1,78 @@
 <?php require_once '../components/header.php' ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Carrinho de Compras</title>
-    <link rel="stylesheet" href="../assets/scss/main.css">
-    <script src="../js/bootstrap/bootstrap.bundle.min.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Carrinho de Compras</title>
+  <link rel="stylesheet" href="../assets/scss/main.css">
+  <script src="../js/bootstrap/bootstrap.bundle.min.js"></script>
 </head>
+
 <body class="background-dark-light">
-<div class="container text-start mt-5">
-  <div class="row">
-    <div class="col">
-      <h2 class="lexend-title-semibold text-warning">Carrinho de Compras</h2>
-      <div class="shadow py-3 ps-3 mt-5 mb-5 text-warning bg-dark rounded">
-      <div class="row align-items-start">
-    <div class="col">
-      <img src="https://placehold.co/100" alt="">
-    </div>
-    <div class="col-9">
-      <p>Nome do Livro: livro</p>
-      <p class="text-light">Autor: autor</p>
-      <p class="text-light">Editora: editora</p>
-      <p class="lexend-text-medium fs-5">R$ XX,XX</p>
-    </div>
-    <div class="col">
-        <a href="" class="btn btn-warning btn-lg text-light">Ver Produto</a>
-        <a href="" class="btn btn-primary btn-lg text-light">sass</a>
-    </div>
-      </div>
+  <div class="container text-start text-light mt-5">
+    <h2 class="lexend-title-semibold text-warning">Carrinho de Compras</h2>
+    <div class="row shadow p-3 my-5 my-sm-4 bg-dark rounded">
+      <div class="col-md-4">
+        <div class="row">
+          <div class="col">
+            <img src="https://placehold.co/100x150" alt="">
+          </div>
+          <div class="col-7">
+            <ul class="navbar-nav justify-content-start">
+              <li class="nav-item">
+                <p class="fs-5 lexend-title-semibold text-warning">Nome do Livro</p>
+              </li>
+              <li class="nav-item">
+                <p>Autor: Autor</p>
+              </li>
+              <li class="nav-item">
+                <p>Editora: Editora</p>
+              </li>
+              <li class="nav-item">
+                <p class="fs-5 lexend-title-semibold text-warning">R$ XX,XX</p>
+              </li>
+            </ul>
+          </div>
         </div>
+      </div>
+      <div class="col-6 col-md-8 align-self-end text-end">
+        <a href="" class="btn btn-warning btn-lg text-light me-2">Ver Produto</a>
+        <a href="" class="btn btn-danger btn-lg">Remover Produto</a>
+      </div>
+    </div>
+    <div class="row shadow p-3 my-5 my-sm-5 bg-dark rounded">
+      <div class="col-md-4">
+        <div class="row">
+          <div class="col">
+            <img src="https://placehold.co/100x150" alt="">
+          </div>
+          <div class="col-7">
+            <ul class="navbar-nav justify-content-start">
+              <li class="nav-item">
+                <p class="fs-5 lexend-title-semibold text-warning">Nome do Livro</p>
+              </li>
+              <li class="nav-item">
+                <p>Autor: Autor</p>
+              </li>
+              <li class="nav-item">
+                <p>Editora: Editora</p>
+              </li>
+              <li class="nav-item">
+                <p class="fs-5 lexend-title-semibold text-warning">R$ XX,XX</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 col-md-8 align-self-end text-end">
+        <a href="" class="btn btn-warning btn-lg text-light me-2">Ver Produto</a>
+        <a href="" class="btn btn-danger btn-lg">Remover Produto</a>
       </div>
     </div>
   </div>
-</div>
 </body>
 <?php require_once '../components/footer.php' ?>
+
 </html>
