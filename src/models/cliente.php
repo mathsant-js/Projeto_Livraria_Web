2c5a5a01eb858413df7991fa705d649d7572b90e
Lucas-Tino <lucas.rosa131@etec.sp.gov.br>
2024-10-22 12:03:00 -0300

    feat: added livro model class

---
+++ src/models/cliente.php
@@ -0,0 +1,109 @@
+<?php 
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/Projeto_Livraria_Web/connection/conexao.php';
+
+class Cliente {
+    // atributos
+    private $codCliente;
+    private $nomeCliente;
+    private $cpfCliente;
+    private $datanascCliente;
+    private $senhaCliente;
+
+    // gets e sets
+    public function getCodCliente() {
+        return $this->codCliente;
+    }
+
+    public function setCodCliente($codCliente) {
+        $this->codCliente = $codCliente;
+    }
+
+    public function getNomeCliente() {
+        return $this->nomeCliente;
+    }
+
+    public function setNomeCliente($nomeCliente) {
+        $this->nomeCliente = $nomeCliente;
+    }
+
+    public function getCpfCliente() {
+        return $this->cpfCliente;
+    }
+
+    public function setCpfCliente($cpfCliente) {
+        $this->cpfCliente = $cpfCliente;
+    }
+
+    public function getDatanascCliente() {
+        return $this->datanascCliente;
+    }
+
+    public function setDatanascCliente($datanascCliente) {
+        $this->datanascCliente = $datanascCliente;
+    }
+
+    public function getSenhaCliente() {
+        return $this->senhaCliente;
+    }
+
+    public function setSenhaCliente($senhaCliente) {
+        $this->senhaCliente = $senhaCliente;
+    }
+
+    // construtor
+    public function __construct() {
+        $this->conexao = new Conexao();
+    }
+
+    // insert
+    public function inserir() {
+        $sql = "INSERT INTO cliente (`nome_cliente`, `cpf_cliente`, `data_nascimento_cliente`, `senha_cliente`) VALUES (?,?,?,?);";
+        $stmt = $this->conexao->getConexao()->prepare($sql);
+        $stmt->bind_param('ssss', $this->nomeCliente, $this->cpfCliente, $this->datanascCliente, $this->senhaCliente);
+        return $stmt->execute();
+    }
+
+    // listar
+    public function listar() {
+        $sql = "SELECT * FROM cliente";
+        $stmt = $this->conexao->getConexao()->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $clientes = [];
+
+        while ($cliente = $result->fetch_assoc()) {
+            $pessoas[] = $pessoa;
+        }
+
+        return $pessoas;
+    }
+
+    // buscar por id
+    public function buscarPorId($codCliente) {
+        $sql = "SELECT * FROM cliente WHERE cod_cliente = ?";
+        $stmt = $this->conexao->getConexao()->prepare($sql);
+        $stmt->bind_param('i', $codCliente);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_assoc();
+    }
+
+    // update
+    public function atualizar($codCliente) {
+        $sql = "UPDATE cliente SET nome_cliente = ?, cpf_cliente = ?, data_nascimento_cliente = ?, senha_cliente = ? WHERE cod_cliente = ?";
+        $stmt = $this->conexao->getConexao()->prepare($sql);
+        $stmt->bind_param('ssssi', $this->nomeCliente, $this->cpfCliente, $this->datanascCliente, $this->senhaCliente, $codCliente);
+        return $stmt->execute();
+    }
+
+    // delete
+    public function excluir($codCliente) {
+        $sql = "DELETE FROM cliente WHERE cod_cliente = ?";
+        $stmt = $this->conexao->getConexao()->prepare($sql);
+        $stmt->bind_param('i', $codCliente);
+        return $stmt->execute();
+    }
+}
+
+?>
