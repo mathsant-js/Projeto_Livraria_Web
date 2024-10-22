a8305c93832289b6b8465df75fe09959aca5acc4
mathsant-js <matheus.santana114@etec.sp.gov.br>
2024-10-22 12:18:02 -0300

    feat: CRUD has been added to the generos.php class

--- src/models/genero.php
+++ src/models/genero.php
@@ -6,6 +6,7 @@ class Genero {
     private $codGenero;
     private $nomeGenero;
     private $descricaoGenero;
+    private $conexao;
 
     public function getCodGenero() {
         return $this->codGenero;
@@ -29,5 +30,59 @@ class Genero {
 
     public function setDescricaoGenero($descricaoGenero) {
         $this->descricaoGenero = $descricaoGenero;
+    }
+
+    // construtor
+    public function __construct() {
+        $this->conexao = new Conexao();
+    }
+
+    // insert
+    public function inserir() {
+        $sql = "INSERT INTO genero (`nome_genero`, `descricao_genero`) VALUES (?,?);";
+        $stmt = $this->conexao->getConexao()->prepare($sql);
+        $stmt->bind_param('ss', $this->nomeGenero, $this->descricaoGenero);
+        return $stmt->execute();
+    }
+
+    // listar
+    public function listar() {
+        $sql = "SELECT * FROM genero";
+        $stmt = $this->conexao->getConexao()->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $generos = [];
+
+        while ($genero = $result->fetch_assoc()) {
+            $generos[] = $genero;
+        }
+
+        return $generos;
+    }
+
+    // buscar por id
+    public function buscarPorId($codGenero) {
+        $sql = "SELECT * FROM genero WHERE cod_genero = ?";
+        $stmt = $this->conexao->getConexao()->prepare($sql);
+        $stmt->bind_param('i', $codGenero);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_assoc();
+    }
+
+    // update
+    public function atualizar($codGenero) {
+        $sql = "UPDATE genero SET nome_genero = ?, descricao_genero = ? WHERE cod_genero = ?";
+        $stmt = $this->conexao->getConexao()->prepare($sql);
+        $stmt->bind_param('ssssi', $this->nomeGenero, $this->descricaoGenero, $codGenero);
+        return $stmt->execute();
+    }
+
+    // delete
+    public function excluir($codGenero) {
+        $sql = "DELETE FROM genero WHERE cod_genero = ?";
+        $stmt = $this->conexao->getConexao()->prepare($sql);
+        $stmt->bind_param('i', $codGenero);
+        return $stmt->execute();
     }
 }
