154461e472a0e9c3e4d4e04db0350da373abc811
matheussantana2007 <matheus.santana114@etec.sp.gov.br>
2024-11-13 22:40:17 -0300

    feat: CRUD was added in livro.php

--- src/models/livro.php
+++ src/models/livro.php
@@ -9,6 +9,7 @@ class Livro {
     private $dataLancamento;
     private $precoLivro;
     private $descricaoLivro;
+    private $conexao;
 
     public function getCodLivro() {
         return $this->codLivro;
@@ -56,5 +57,60 @@ class Livro {
 
     public function setDescricaoLivro($descricaoLivro) {
         $this->descricaoLivro = $descricaoLivro;
+    }
+
+    // construtor
+    public function __construct() {
+        $this->conexao = new Conexao();
+    }
+
+    // insert
+    public function inserir() {
+        $sql = "INSERT INTO livro (`nome_livro`, `isbn_livro`, `data_lancamento_livro`, `preco_livro`, `descricao_livro`) VALUES (?,?,?,?,?);";
+        $stmt = $this->conexao->getConexao()->prepare($sql);
+        $stmt->bind_param('sssss', $this->nomeLivro, $this->isbnLivro, $this->dataLancamento, $this->precoLivro, $this->descricaoLivro);
+        $stmt->execute();
+        return $stmt->insert_id;
+    }
+
+    // listar
+    public function listar() {
+        $sql = "SELECT * FROM livro";
+        $stmt = $this->conexao->getConexao()->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $livros = [];
+
+        while ($livro = $result->fetch_assoc()) {
+            $livros[] = $livro;
+        }
+
+        return $livros;
+    }
+
+    // buscar por id
+    public function buscarPorId($codLivro) {
+        $sql = "SELECT * FROM livro WHERE cod_livro = ?";
+        $stmt = $this->conexao->getConexao()->prepare($sql);
+        $stmt->bind_param('i', $codLivro);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_assoc();
+    }
+
+    // update
+    public function atualizar($codLivro) {
+        $sql = "UPDATE livro SET nome_livro = ?, isbn_livro = ?, data_lancamento = ?, preco_livro = ?, descricao_livro = ? WHERE cod_livro = ?";
+        $stmt = $this->conexao->getConexao()->prepare($sql);
+        $stmt->bind_param('sssssi', $this->nomeLivro, $this->isbnLivro, $this->dataLancamento, $this->precoLivro, $codLivro);
+        return $stmt->execute();
+    }
+
+    // delete
+    public function excluir($codLivro) {
+        $sql = "DELETE FROM livro WHERE cod_livro = ?";
+        $stmt = $this->conexao->getConexao()->prepare($sql);
+        $stmt->bind_param('i', $codLivro);
+        return $stmt->execute();
     }
 }
